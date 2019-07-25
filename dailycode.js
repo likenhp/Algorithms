74e3ec93b6a9fe821d0420382797b13d0c58a31c
Kenneth Li <likenhp@gmail.com>
2019-07-24 21:04:08 -0700

    practice

--- dailycode.js
+++ dailycode.js
@@ -1,4 +1,6 @@
 /*
+Problem 1
+
 Given a list of numbers and a number k, 
 return whether any two numbers from the list add up to k.
 
@@ -30,3 +32,30 @@ function findSum (nums, target){
 }
 
 findSum([10, 15, 3, 7], 17);
+
+/*
+Problem 2
+
+Given an array of integers, 
+return a new array such that each element at index i 
+of the new array is the product of all the numbers in the original array 
+except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], 
+the expected output would be [120, 60, 40, 30, 24]. 
+If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+*/
+
+function multiplyIndex(nums){
+    for (var index = 0; index < nums.length; index ++){
+        var copyArray = nums.slice(0);
+        var temp = nums[index];
+        if(nums[index-1] === undefined){
+            
+        }
+    }
+}
+
+multiplyIndex([1, 2, 3, 4, 5]);
