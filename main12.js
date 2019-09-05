1df46533272105096f79016bc850dec534d4bedb
Kenneth Li <likenhp@gmail.com>
2019-09-04 21:08:02 -0700

    practice

--- main12.js
+++ main12.js
@@ -1,3 +1,18 @@
 /*
 Practice
+
+Best Time to Buy and Sell Stock III
+
+array for which ith element is the price of a given stock on day i
+design an algorithm to find max profit, can complete at most 2  transactions
+
+cannot engage in multiple transactions at the same time (ie. must sell stock before buying again)
+
+Input: [3,3,5,0,0,3,1,4]
+Output: 6
+
 */
+
+var maxProfit = function(prices) {
+    
+};
