2d5eb10e9bb89d3d07f185a679504fbb62991088
likenhp <likenhp@gmail.com>
2020-11-18 22:46:35 -0800

    11-18

--- practice.js
+++ practice.js
@@ -211,5 +211,17 @@ var validPalindrome = function(s, skipped = false) {
  * @return {boolean}
  */
 var isCousins = function(root, x, y) {
-    
+  function getDepthAndParent(node, n, depth=0, parent){
+    if(!node) return null;
+    if(node.val === n) {
+        return {depth, parent};
+    }
+    let left = getDepthAndParent(node.left, n, depth+1, node);
+    let right = getDepthAndParent(node.right, n, depth+1, node);
+    return left || right;
+  }
+  let {depth: xDepth , parent: xParent } = getDepthAndParent(root,x);
+  let {depth: yDepth , parent: yParent } = getDepthAndParent(root,y);
+
+  return xDepth === yDepth && xParent !== yParent;
 };
